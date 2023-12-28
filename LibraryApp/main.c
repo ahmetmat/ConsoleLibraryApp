d374c3de84e20e289fc20174fbba70d495da1f7f
Ahmet Tahir Mat <107697136+ahmetmat@users.noreply.github.com>
2023-12-28 23:05:38 +0300

    Update main.c
    Language updated to English and bugs fixed

--- LibraryApp/main.c
+++ LibraryApp/main.c
@@ -1,21 +1,22 @@
 #include <stdio.h>
 
-typedef struct { // Grup olarak tanımlama
+// Define the structure for a book
+typedef struct {
     int id;
     char bookgenre[50];
     char title[100];
- 
-} Book;// struct türü
+} Book;
 
-
-void listBooks(Book books[], int length) {// books[] arrayini ve o arrayını içerisinden hangi uzunluğa kadar olanı istediğini alır.
-    printf("\nID\tKitap Türü\t\tKitap Adı\n");
+// Function to list books
+void listBooks(Book books[], int length) {
+    printf("\nID\tBook Genre\t\tBook Title\n");
     printf("---------------------------------------------------------\n");
     for (int i = 0; i < length; i++) {
         printf("%-4d\t%-20s\t%-40s\n", books[i].id, books[i].bookgenre, books[i].title);
     }
 }
 
+// Function to find a book by its ID
 Book findBookById(Book books[], int length, int id) {
     for (int i = 0; i < length; i++) {
         if (books[i].id == id) {
@@ -23,115 +24,100 @@ Book findBookById(Book books[], int length, int id) {
         }
     }
     
-    // Eğer kitap bulunamazsa 'id'si -1 olan bir kitap döndürülür.
-    Book emptyBook = {-1, "", ""};
+    Book emptyBook = {-1, "", ""}; // Return an empty book if not found
     return emptyBook;
 }
 
 int main(void) {
-    printf("Oğuzhan Kitabevine hoşgeldiniz!\n");
+    printf("Welcome to Bookstore!\n");
     
+    // Array of books
     Book books[10] = {
-        {1, "Roman", "Suç ve Ceza"},
-        {2, "Roman", "Ezilenler"},
-        {3, "Felsefe", "Devlet"},
-        {4, "Biyografi", "Steve Jobs"},
-        {5, "Biyografi", "Leonardo da Vinci"},
-        {6, "Kişisel Gelişim", "Çok Yönlü"},
-        {7, "Felsefe", "Sokrates'in Savunması"},
-        {8, "Kişisel Gelişim", "İrade Eğitimi"},
-        {9, "Girişimcilik", "Google"},
-        {10, "Bilgisayar", "C Programlama"}
+        {1, "Novel", "Crime and Punishment"},
+        {2, "Novel", "The Oppressed"},
+        {3, "Philosophy", "The Republic"},
+        {4, "Biography", "Steve Jobs"},
+        {5, "Biography", "Leonardo da Vinci"},
+        {6, "Personal Development", "Versatile"},
+        {7, "Philosophy", "The Apology of Socrates"},
+        {8, "Personal Development", "Willpower Training"},
+        {9, "Entrepreneurship", "Google"},
+        {10, "Computer Science", "C Programming"}
     };
     
-    
-    int kitapid, choice, result;
+    int kitapid, choice;
     char onay;
     Book selectedBook;
 
     do {
-        printf("\n1. Kitap Ödünç Al\n2. Kitap teslimi \n3. Çok Okunanlar\n4. Kitapları Listele\n5. Çıkış\n \n Seçiminiz: ");
-        result = scanf("%d", &choice);
-
-        // Input buffer'ını temizle.
-        while (getchar() != '\n');
-
-        if (result != 1 || (choice < 1 || choice > 5)) {
-            // Eğer kullanıcı sayısal olmayan bir giriş yaptıysa veya menü dışında bir seçim yaptıysa.
-            printf("Geçersiz seçenek. Lütfen 1 ile 5 arasında bir sayı giriniz.\n");
+        printf("\n1. Borrow a Book\n2. Return a Book\n3. Best Sellers\n4. List Books\n5. Exit\n \n Your Choice: ");
+        if (scanf("%d", &choice) != 1) {
+            printf("Invalid option. Please enter a number between 1 and 5.\n");
+            // Clear invalid input
+            while (getchar() != '\n');
             continue;
         }
 
         switch(choice) {
             case 1:
                 listBooks(books, 10);
-                printf("Hangi Kitabı almak istiyorsunuz? (ID giriniz): ");
-                result = scanf("%d", &kitapid);
-                
-                if(result != 1) {
-                    printf("Geçersiz ID. Lütfen bir sayı giriniz.\n");
+                printf("Which book would you like to borrow? (Enter ID): ");
+                if (scanf("%d", &kitapid) != 1) {
+                    printf("Invalid ID. Please enter a number.\n");
+                    while (getchar() != '\n');
                     continue;
                 }
                 
                 selectedBook = findBookById(books, 10, kitapid);
                 if (selectedBook.id == -1) {
-                    printf("Seçilen ID'ye sahip bir kitap bulunamadı.\n");
-                    printf("Ana menüye yönlendiriliyorsunuz.\n");
-
+                    printf("No book found with the entered ID.\n");
                 } else {
-                    printf("\nSeçilen Kitap:\nID: %d\nTür: %s\nKitap Adı: %s\nBu kitabı almak istiyor musunuz? (E/H): ",
+                    printf("\nSelected Book:\nID: %d\nGenre: %s\nTitle: %s\nDo you want to borrow this book? (Y/N): ",
                            selectedBook.id, selectedBook.bookgenre, selectedBook.title);
-                    scanf(" %c", &onay);
-                    if(onay == 'E' || onay == 'e') {
-                        printf("Tebrikler! '%s' kitabını ödünç aldınız.\n", selectedBook.title);
-                        printf("Ana menüye yönlendiriliyorsunuz.\n");
-
+                    scanf(" %c", &onay); // Space character will consume the newline in buffer.
+                    if(onay == 'Y' || onay == 'y') {
+                        printf("Congratulations! You have borrowed '%s'.\n", selectedBook.title);
+                    } else if(onay == 'N' || onay == 'n') {
+                        printf("Book borrowing cancelled.\n");
                     } else {
-                        printf("Kitap alma işlemi iptal edildi.\n");
-                        printf("Ana menüye yönlendiriliyorsunuz.\n");
+                        printf("Invalid character. Please enter 'Y' or 'N'.\n");
                     }
                 }
                 break;
             case 2:
-                printf("Hangi kitabınızı teslim etmek istiyorsunuz? id: ");
-                result = scanf("%d", &kitapid);
-                if (result != 1) {
-                    // Eğer kullanıcı sayısal olmayan bir giriş yaptıysa.
-                    printf("Lütfen geçerli bir kitap ID'si giriniz.\n");
-                    printf("Ana menüye yönlendiriliyorsunuz.\n");
-
+                printf("Which book would you like to return? (Enter ID): ");
+                if (scanf("%d", &kitapid) != 1) {
+                    printf("Invalid ID. Please enter a number.\n");
+                    while (getchar() != '\n');
                     continue;
                 }
-                if(kitapid<=10 && kitapid>=0){
+                if (kitapid > 0 && kitapid <= 10) {
                     selectedBook = findBookById(books, 10, kitapid);
-                    printf("Tebrikler! '%s' adlı kitabınız teslim ettiniz.\n", selectedBook.title);
-                    printf("Ana menüye yönlendiriliyorsunuz.\n");
-
-                }
-                else
-                {
-                    printf("Seçilen ID'ye sahip bir kitap bulunamadı.\n");
-                    printf("Ana menüye yönlendiriliyorsunuz.\n");
-
+                    if (selectedBook.id != -1) {
+                        printf("Congratulations! You have returned '%s'.\n", selectedBook.title);
+                    } else {
+                        printf("No book found with the entered ID.\n");
+                    }
+                } else {
+                    printf("Invalid ID. Please enter a valid book ID.\n");
                 }
                 break;
+
             case 3:
-                listBooks(books, 5);
+                listBooks(books, 5); // List top 5 books
                 break;
             case 4:
-                listBooks(books, 10); // Kitapları listeleme fonksiyonu çağrılıyor
+                listBooks(books, 10); // List all books
                 break;
             case 5:
-                printf("Sağlıklı günler, iyi okumalar...\n");
-
-                printf("Sistemden çıkılıyor...\n");
+                printf("Have healthy days, happy reading...\n");
+                printf("Exiting the system...\n");
                 break;
             default:
-                printf("Geçersiz seçenek. Lütfen 1 ile 5 arasında bir sayı giriniz.\n");
+                printf("Invalid option. Please enter a number between 1 and 5.\n");
                 break;
         }
     } while(choice != 5);
 
     return 0;
 }
-
